78044d2c619bdc32abc37ef053bf238982e170bc
Matei stefanita <stefanita.matei@emanuel.ro>
2023-10-24 18:06:25 +0300

    user-parola

---
+++ Tema/Tema/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Tema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string usernamecorect = "fastan";
+            const string passwordcorect = "depend";
+
+            bool autentifacare = false;
+
+            while(!autentifacare)
+            {
+                Console.WriteLine("Introduceti username: ");
+                string username=Console.ReadLine();
+                Console.WriteLine("Introduceti password: ");
+                string password=Console.ReadLine();
+                if (username == usernamecorect && password == passwordcorect) 
+                {
+                    Console.WriteLine("Bine ai venit, " + usernamecorect);
+                    autentifacare=true;
+                }
+                else
+                {
+                    Console.WriteLine("Autentificare esuata. Incercati din nou. ");
+
+                }
+            }
+        }
+    }
+}
